ccd395c2f5500e135a1ff9714ac62a79c6214314
NameArtid <artid.s@cs.rmu.ac.th>
2024-02-19 22:17:45 +0700

    update shop page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Navbar } from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
-import ShopCatagory from "./Pages/ShopCatagory";
+import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
@@ -13,15 +14,16 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCatagory category="mens" />} />
-          <Route path="/womans" element={<ShopCatagory category="womans" />} />
-          <Route path="/kids" element={<ShopCatagory category="kids" />} />
+          <Route path="/mans" element={<ShopCategory category="man" />} />
+          <Route path="/womans" element={<ShopCategory category="woman" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":product" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/Pages/ShopCatagory.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ShopCatagory() {
-  return (
-    <div>ShopCatagory</div>
-  )
-}
